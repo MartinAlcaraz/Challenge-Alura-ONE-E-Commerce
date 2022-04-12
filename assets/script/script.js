c4f54d21bd5468582cfe4d9e90bc0bbf6094c692
MartinAlcaraz <96241107+MartinAlcaraz@users.noreply.github.com>
2022-04-12 10:14:49 -0300

    enlaces a index agregados

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,17 +1,17 @@
 const boton_login = document.querySelector("#boton-login");
-const boton_home = document.querySelector("#menu-logo");
+const boton_home = document.querySelector(".home");
 
 const bloque_login = document.querySelector("#bloque-login");
 const bloque_banner = document.querySelector("#bloque-banner");
 const bloque_productos = document.querySelector("#bloque-productos");
 
 
-boton_home.addEventListener("click", ()=>{
-    bloque_banner.classList.remove("banner--invisible");
-    bloque_productos.classList.remove("bloque__productos--invisible");
-    bloque_login.classList.add("bloque__login--invisible");    
+// boton_home.addEventListener("click", ()=>{
+//     bloque_banner.classList.remove("banner--invisible");
+//     bloque_productos.classList.remove("bloque__productos--invisible");
+//     bloque_login.classList.add("bloque__login--invisible");    
 
-});
+// });
 
 boton_login.addEventListener("click", () => {
     bloque_login.classList.remove("bloque__login--invisible");
