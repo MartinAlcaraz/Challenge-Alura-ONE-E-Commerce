6e70931b62ad75408e9186efc8486875a911813f
MartinAlcaraz <96241107+MartinAlcaraz@users.noreply.github.com>
2022-04-23 11:50:10 -0300

    arreglado seccion de contacto y buscador del menú

--- assets/script/login.js
+++ assets/script/login.js
@@ -1,5 +1,5 @@
 import { validar } from "./validaciones.js";
-
+import { addEventToInputsContacto } from "./script.js";
 
 const inputs_login = document.querySelectorAll(".input-login");
 const boton_entrar_login = document.querySelector("[data-submit-login]");
@@ -8,24 +8,9 @@ const inputs_contacto = document.querySelectorAll(".input-contacto");
 const boton_enviar_contacto = document.querySelector("[data-submit-contacto]");
 
 
-
 // validacion de formulario de contacto
-inputs_contacto.forEach((input) => {
 
-    input.addEventListener("blur", (input) => {
-        validar(input.target)
-    });
-
-    input.addEventListener("keyup", (input) => {    // comprueba despues de ingresar un caracter del teclado
-
-        // if ( tipoDeInput == "nombre"){        // comprueba solo el campo data-tipo="nombre" en cada ingreso de teclado
-        //       validar(input.target);
-        // }   
-        validar(input.target);
-        //habilitarBotonSubmit(inputs_contacto, boton_enviar_contacto);
-    });
-});
-
+addEventToInputsContacto(inputs_contacto, boton_enviar_contacto);
 
 
 // validacion de formulario login
@@ -59,8 +44,6 @@ boton_entrar_login.addEventListener("click", (event) => {
         window.open("./todos-los-productos.html", "_self");
         // window.location.replace("./todos-los-productos.html");    alternativas para abrir una pagina
         // location.href="./todos-los-productos.html";  
-
     }
-    
 
 });
--- assets/script/script.js
+++ assets/script/script.js
@@ -5,26 +5,33 @@ const inputs_contacto = document.querySelectorAll(".input-contacto");
 const boton_enviar_contacto = document.querySelector("[data-submit-contacto]");
 
 
+// validacion de formularios de contacto 
 
-// validacion de formulario de contacto
-inputs_contacto.forEach((input) => {
+export const addEventToInputsContacto = (inputs, boton_enviar) => {
 
-    input.addEventListener("blur", (input) => {
-        validar(input.target)
+    inputs.forEach((input) => {
+
+        input.addEventListener("blur", (input) => {
+            validar(input.target)
+        });
+
+        input.addEventListener("keyup", (input) => {    // comprueba despues de ingresar un caracter del teclado
+
+            // if ( tipoDeInput == "nombre"){        // comprueba solo el campo data-tipo="nombre" en cada ingreso de teclado
+            //       validar(input.target);
+            // }   
+            validar(input.target);
+            //habilitarBotonSubmit(inputs_contacto, boton_enviar_contacto);
+        });
     });
 
-    input.addEventListener("keyup", (input) => {    // comprueba despues de ingresar un caracter del teclado
-
-        // if ( tipoDeInput == "nombre"){        // comprueba solo el campo data-tipo="nombre" en cada ingreso de teclado
-        //       validar(input.target);
-        // }   
-        validar(input.target);
-        //habilitarBotonSubmit(inputs_contacto, boton_enviar_contacto);
+    boton_enviar.addEventListener("click", () => {
+        inputs.forEach((input) => {
+            validar(input);
+        });
     });
-});
+}
 
-boton_enviar_contacto.addEventListener("click", () => {
-    inputs_contacto.forEach((input) => {
-        validar(input);
-    });
-});
+addEventToInputsContacto(inputs_contacto, boton_enviar_contacto);
+
+
